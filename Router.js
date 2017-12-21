b916ad3ebbbe1b464e4f94a8a5cc2a403ab5c8eb
Andrei Biasprozvanny <abiaspro@thoughtworks.com>
2017-12-21 11:22:24 +0000

    Adds PetitionSummary screen to flow

--- Router.js
+++ Router.js
@@ -3,10 +3,12 @@ import {
 } from '@expo/ex-navigation';
 import Home from './screens/Home';
 import Authorisation from './screens/Authorisation';
+import PetitionSummary from './screens/PetitionSummary';
 
 const Router = createRouter(() => ({
   home: () => Home,
   authorisation: () => Authorisation,
+  petitionSummary: () => PetitionSummary,
 }));
 
 export default Router;
--- screens/Authorisation.js
+++ screens/Authorisation.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Image, Text, View, Button, FlatList } from 'react-native';
+import PropTypes from 'prop-types';
+import Router from '../Router';
 
 const decodeLogo = require('../assets/images/wallet_logo.png');
 
@@ -37,35 +39,48 @@ const styles = StyleSheet.create({
   },
 });
 
-function Authorisation() {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={decodeLogo}
-      />
-      <View style={styles.textContainer}>
-        <Text style={styles.textHeading}>Secure Petitions is requesting
-        to connect with your DECODE wallet</Text>
-        <Text style={styles.textParagraph}>Secure Petitions may request access to
-        the following information stored in your wallet.</Text>
-        <FlatList
-          style={styles.flatList}
-          data={[{ key: '- Atlantis Residency' }]}
-          renderItem={({ item }) => <Text>{item.key}</Text>}
+export default class Authorisation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.goToPetitionSummary = this.goToPetitionSummary.bind(this);
+  }
+
+  goToPetitionSummary() {
+    this.props.navigator.push(Router.getRoute('petitionSummary'));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={decodeLogo}
         />
-        <Text>Accepting this connection will not automatically share
-        this information with Secure Petitions.</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.textHeading}>Secure Petitions is requesting
+            to connect with your DECODE wallet</Text>
+          <Text style={styles.textParagraph}>Secure Petitions may request access to
+            the following information stored in your wallet.</Text>
+          <FlatList
+            style={styles.flatList}
+            data={[{ key: '- Atlantis Residency' }]}
+            renderItem={({ item }) => <Text>{item.key}</Text>}
+          />
+          <Text>Accepting this connection will not automatically share
+            this information with Secure Petitions.</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this.goToPetitionSummary}
+            title="Authorise Connection"
+            color="rgb(0,163,158)"
+          />
+        </View>
       </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          onPress={() => true}
-          title="Authorise Connection"
-          color="rgb(0,163,158)"
-        />
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
-export default Authorisation;
+Authorisation.propTypes = {
+  navigator: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+};
