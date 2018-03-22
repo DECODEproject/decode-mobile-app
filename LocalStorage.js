9f4b72e879c6d4643e087461be4073695f8f8f64
Jim Barritt <jimb@thoughtworks.com>
2018-03-22 12:03:40 +0000

    Adding milagro and generated a keypair as wallet id

--- LocalStorage.js
+++ LocalStorage.js
@@ -1,6 +1,8 @@
 import uuidv1 from 'uuid';
 import { AsyncStorage } from 'react-native';
 
+import CTX from 'milagro-crypto-js'
+
 export const getWalletID = async () => {
   let result;
   try {
@@ -11,11 +13,30 @@ export const getWalletID = async () => {
   return result;
 };
 
+function generateWalletId() {
+    var S0 = [];
+    var W0 = [];
+
+   var ctx2 = new CTX("NIST256");
+
+  var rng = new ctx2.RAND();
+
+    //S0 - private key
+    //W0 - public key
+   ctx2.ECDH.KEY_PAIR_GENERATE(rng, S0, W0);
+
+  //  return W0;
+    return ctx2.ECDH.bytestostring(W0);
+
+
+}
+
 export const initialiseWalletID = async () => {
   const id = await getWalletID();
-  if (!id) {
+  if (id) {
     try {
-      await AsyncStorage.setItem('@MyStore:id', uuidv1());
+
+      await AsyncStorage.setItem('@MyStore:id', generateWalletId());
     } catch (e) {
       console.error(e);
     }
