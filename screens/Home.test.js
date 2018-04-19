50653787691abe60bd61f76e9bcfde6a500445e0
Elisa Cutrin <ecutrin@thoughtworks.com>
2018-04-19 14:30:25 +0200

    WIP- Fixing tests

--- screens/Home.test.js
+++ screens/Home.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from './Home';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([]);
+
+
+const initialState = {
+  petitionLink: {
+    petitionLink: undefined
+  },
+};
+
 
 it('renders Home component', () => {
-  const rendered = renderer.create(<Home navigator={{ push: () => {} }} />).toJSON();
-  expect(rendered).toMatchSnapshot();
+  const wrapper = shallow(
+    <Home />,
+    { context: { store: mockStore(initialState) } },
+  );
+  expect(wrapper.dive()).toMatchSnapshot();
+
+  //const rendered = renderer.create(<Home />).toJSON();
+  //expect(rendered).toMatchSnapshot();
 });
