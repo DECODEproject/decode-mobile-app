5e54b439b17c77dbde7ab2921f656d4b5d70d74e
Elisa Cutrin <ecutrin@thoughtworks.com>
2018-04-26 09:53:23 +0200

    [+Albert] #5 - Added test for error in getPetition action

--- test/application/redux/actions/petition.test.js
+++ test/application/redux/actions/petition.test.js
@@ -7,23 +7,11 @@ import { types } from '../../../../application/redux/actionTypes';
 const mockStore = configureMockStore([thunk]);
 
 describe('getPetition', () => {
-  it('should dispatch successful action', () => {
-    const petitionLink = 'petitions';
-    const newPetition = {
-      petition: {
-        id: '2',
-        isEthereum: 'true',
-      },
-    };
-    const response = newPetition;
+  const petitionLink = 'petitions';
+  let store;
 
-    const expectedActions = [{
-      type: types.SET_PETITION,
-      petition: newPetition,
-    }];
-
-    fetchMock.getOnce(petitionLink, response);
-    const store = mockStore({
+  beforeEach(() => {
+    store = mockStore({
       petition: {
         petition: {
           id: '1234',
@@ -34,6 +22,44 @@ describe('getPetition', () => {
         id: 'my-wallet-id-numbeeeeeeeeeeer',
       },
     });
+  });
+
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
+  it('should dispatch successful action', () => {
+    const newPetition = {
+      petition: {
+        id: '2',
+        isEthereum: 'true',
+      },
+    };
+    const response = newPetition;
+    const expectedActions = [{
+      type: types.SET_PETITION,
+      petition: newPetition,
+    }];
+
+    fetchMock.getOnce(petitionLink, response);
+
+    return store.dispatch(getPetition(petitionLink)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('should dispatch error action', () => {
+    const errorMessage = 'Internal Server Error';
+
+    const expectedActions = [{
+      type: types.SET_PETITION_ERROR,
+      error: errorMessage,
+    }];
+
+    const response = { status: 500 };
+
+    fetchMock.getOnce(petitionLink, response);
 
     return store.dispatch(getPetition(petitionLink)).then(() => {
       expect(store.getActions()).toEqual(expectedActions);
