4182f59b5e4bd26c31fab15a566a2acfa7010c2d
Elisa Cutrin <ecutrin@thoughtworks.com>
2018-05-31 14:51:28 +0200

    [+MOB] WIP of Signing acceptance test

--- test/acceptance/sign.test.js
+++ test/acceptance/sign.test.js
@@ -1,6 +1,84 @@
-describe('', () => {
-  it('', () => {});
+import React from 'react';
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import thunk from 'redux-thunk';
+import PetitionSummary from '../../screens/PetitionSummary';
+// import Button from '../../application/components/Button/Button';
+// import { jsdom } from 'jsdom'
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockStore = configureMockStore([thunk]);
+
+
+// global.document = jsdom('');
+// global.window = document.defaultView;
+//
+// Object.keys(document.defaultView).forEach((property) => {
+//   if (typeof global[property] === 'undefined') {
+//     global[property] = document.defaultView[property];
+//   }
+// });
+
+describe('signing a petition', () => {
+  let initialState;
+  let store;
+
+  beforeEach(() => {
+    initialState = {
+      petitionLink: {
+        petitionLink: 'http://something.com',
+      },
+      petition: {
+      },
+      wallet: {
+        id: 'something',
+      },
+      attributes: {
+        isRequiredAttributeEnabled: true,
+        list: [],
+      },
+    };
+
+    store = mockStore(initialState);
+  });
+
+
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
+  it('should not allow me to sign if required attributes are there', () => {
+    // GIVEN a petition with X attributes
+    const newPetition = {
+      petition: {
+        id: '2',
+        isEthereum: 'true',
+      },
+    };
+
+    fetchMock.getOnce(initialState.petitionLink.petitionLink, newPetition);
+
+    // WHEN
+    const wrapper = shallow(
+      <PetitionSummary />,
+      { context: { store } },
+    );
+    console.log(wrapper);
+
+
+    // THEN
+
+    // expect(wrapper.find(Button).length).toEqual(0);
+  });
 });
+
+// NEXT ITERATIONS OF THE TEST:
+// Use library cucumber style
+
+
 // clearState
 
 // before()
