243694bc5e83a15534ddcdc69679c4e0965753f5
Elena Cristina Raffay Iglesias <eraffay@thoughtworks.com>
2018-06-13 14:44:18 +0200

    [+Jordi] #163 - Add environment handling for external devices

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
+import { Constants } from 'expo';
+import urlParse from 'url-parse';
+
 export default function getWalletProxyUrl(environment) {
   const walletProxyUrl = {
     dev: 'http://localhost:5010',
     production: 'http://ec2-54-194-123-109.eu-west-1.compute.amazonaws.com',
   };
+  const hostName = urlParse(Constants.linkingUri, true).hostname;
+  walletProxyUrl.dev = walletProxyUrl.dev.replace('localhost', hostName);
 
   return environment === 'production' ? walletProxyUrl.production : walletProxyUrl.dev;
 }
--- test/unit/config.test.js
+++ test/unit/config.test.js
@@ -1,10 +1,21 @@
+import { Constants } from 'expo';
 import getWalletProxyUrl from '../../config';
 
 describe('getWalletProxyUrl', () => {
-  it('should return dev url if passed nothing', () => {
+  const originalUri = Constants.linkingUri;
+
+  beforeEach(() => {
+    Constants.linkingUri = 'http://someurl.com';
+  });
+
+  afterEach(() => {
+    Constants.linkingUri = originalUri;
+  });
+
+  it('should return url in the current host if passed nothing', () => {
     const walletProxyUrl = getWalletProxyUrl();
 
-    const walletProxyUrlDev = 'http://localhost:5010';
+    const walletProxyUrlDev = 'http://someurl.com:5010';
 
     expect(walletProxyUrl).toEqual(walletProxyUrlDev);
   });
@@ -20,7 +31,7 @@ describe('getWalletProxyUrl', () => {
   it('should return dev url if passed garbage', () => {
     const walletProxyUrl = getWalletProxyUrl('garbage');
 
-    const walletProxyUrlDev = 'http://localhost:5010';
+    const walletProxyUrlDev = 'http://someurl.com:5010';
 
     expect(walletProxyUrl).toEqual(walletProxyUrlDev);
   });
