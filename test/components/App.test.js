c398ff606efdc24646eb0901a0380f5e10d58e01
Elena Cristina Raffay Iglesias <eraffay@thoughtworks.com>
2018-07-23 16:43:29 +0200

    [+Elisa] - #179 - Decide walkthrough or home

--- test/components/App.test.js
+++ test/components/App.test.js
@@ -4,20 +4,34 @@ import Adapter from 'enzyme-adapter-react-16';
 import App from '../../App';
 import Router from '../../Router';
 
+
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
   describe('go to initial screen', () => {
-    it('should go to walkthrough if no pin has been saved', () => {
+    it('should go to walkthrough if no pin has been saved', async () => {
+      const getItemAsync = jest.fn().mockReturnValue(Promise.resolve(undefined));
+      const routerSpy = jest.spyOn(Router, 'getRoute');
+
       const wrapper = shallow(<App />);
 
       const appComponent = wrapper.instance();
 
-      const routerSpy = jest.spyOn(Router, 'getRoute');
 
-      appComponent.goToInitialScreen();
-
+      await appComponent.goToInitialScreen(getItemAsync);
       expect(routerSpy).toBeCalledWith('walkthrough');
+    });
+
+    it('should go to home screen login if pin has been saved', async () => {
+      const routerSpy = jest.spyOn(Router, 'getRoute');
+      const wrapper = shallow(<App />);
+      const appComponent = wrapper.instance();
+
+      const getItemAsync = jest.fn().mockReturnValue(Promise.resolve('1234'));
+
+
+      await appComponent.goToInitialScreen(getItemAsync);
+      expect(routerSpy).toBeCalledWith('home');
     });
   });
 });
