a12b1d5c0848358ec8ae4de081016470ff714680
Raul Vega del Valle <raulvv@Rauls-MacBook-Pro.local>
2018-07-26 14:48:47 +0200

    [+Elisa] #180 add new walkthrough snapshot

---
+++ test/snapshot/Walkthrough.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import configureStore from 'redux-mock-store';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import thunk from 'redux-thunk';
+import Walkthrough from '../../screens/Walkthrough';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockStore = configureStore([thunk]);
+
+const initialState = {
+  navigation: {},
+};
+
+
+it('renders Walkthrough component', () => {
+  const wrapper = shallow(
+    <Walkthrough />,
+    { context: { store: mockStore(initialState) } },
+  );
+  expect(wrapper.dive()).toMatchSnapshot();
+});
