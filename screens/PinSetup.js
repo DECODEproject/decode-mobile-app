ca41f63d7b831497d54f4a7fd482c8420d5df9d7
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-07-26 09:46:04 +0200

    ADD keyboard not overlap save button

--- screens/PinSetup.js
+++ screens/PinSetup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { SecureStore } from 'expo';
 import PropTypes from 'prop-types';
-import { Image, TextInput, View, Text } from 'react-native';
+import { Image, TextInput, View, Text, KeyboardAvoidingView } from 'react-native';
 import { goToAttributesLanding } from '../application/redux/actions/navigation';
 import Button from '../application/components/Button/Button';
 import { changeText1, changeText2, storePin } from '../application/redux/actions/pinSetup';
@@ -11,52 +11,57 @@ import styles from './styles';
 const decodeLogo = require('../assets/images/decode-hexagon.png');
 
 const PinSetup = props => (
-  <View style={styles.pinContainer}>
-    <Image
-      style={styles.pinLogo}
-      source={decodeLogo}
-    />
-
-    <Text style={styles.pinTitle}>
-      Protect Your wallet
-    </Text>
-    <Text style={styles.pinSubtitle}>
-      Let&#39;s setup a pin, so no one else can access your data
-    </Text>
-
-    <Text style={styles.pinInputLabel}>
-      Enter PIN:
-    </Text>
-    <TextInput
-      style={styles.pinPassword}
-      placeholder=" At least 4 digits"
-      keyboardType="numeric"
-      secureTextEntry
-      underlineColorAndroid="transparent"
-      value={props.pin1}
-      onChangeText={pin => props.changeText1(pin)}
-    />
-    <Text style={styles.pinInputLabel}>
-      Confirm Pin:
-    </Text>
-    <TextInput
-      style={styles.pinPassword}
-      placeholder=" Confirm pin"
-      keyboardType="numeric"
-      secureTextEntry
-      underlineColorAndroid="transparent"
-      value={props.pin2}
-      onChangeText={pin => props.changeText2(pin)}
-    />
-    <View style={{ flexDirection: 'row' }}>
-      <Button
-        name="Save"
-        onPress={() => props.storePin(props.pin1)}
-        style={styles.pinButton}
-        enabled={props.valid}
+  <KeyboardAvoidingView
+    behavior="position"
+    keyboardVerticalOffset={50}
+  >
+    <View style={styles.pinContainer}>
+      <Image
+        style={styles.pinLogo}
+        source={decodeLogo}
       />
+
+      <Text style={styles.pinTitle}>
+        Protect Your wallet
+      </Text>
+      <Text style={styles.pinSubtitle}>
+        Let&#39;s setup a pin, so no one else can access your data
+      </Text>
+
+      <Text style={styles.pinInputLabel}>
+        Enter PIN:
+      </Text>
+      <TextInput
+        style={styles.pinPassword}
+        placeholder=" At least 4 digits"
+        keyboardType="numeric"
+        secureTextEntry
+        underlineColorAndroid="transparent"
+        value={props.pin1}
+        onChangeText={pin => props.changeText1(pin)}
+      />
+      <Text style={styles.pinInputLabel}>
+        Confirm Pin:
+      </Text>
+      <TextInput
+        style={styles.pinPassword}
+        placeholder=" Confirm pin"
+        keyboardType="numeric"
+        secureTextEntry
+        underlineColorAndroid="transparent"
+        value={props.pin2}
+        onChangeText={pin => props.changeText2(pin)}
+      />
+      <View style={{ flexDirection: 'row' }}>
+        <Button
+          name="Save"
+          onPress={() => props.storePin(props.pin1)}
+          style={styles.pinButton}
+          enabled={props.valid}
+        />
+      </View>
     </View>
-  </View>
+  </KeyboardAvoidingView>
 );
 
 PinSetup.propTypes = {
