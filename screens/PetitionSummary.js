272a76aa83fd942507620b95ee03fd196c9da5ca
Raul Vega del Valle <raulvv@Rauls-MacBook-Pro.local>
2018-08-07 12:09:40 +0200

    [+Jordi] #196 - removing prop petitionAttributes from component

--- screens/PetitionSummary.js
+++ screens/PetitionSummary.js
@@ -151,7 +151,6 @@ PetitionSummary.propTypes = {
   petitionError: PropTypes.string,
   getPetition: PropTypes.func.isRequired,
   walletId: PropTypes.string.isRequired,
-  petitionAttributes: PropTypes.arrayOf(PropTypes.shape({})),
   attributes: PropTypes.shape({
     isRequiredAttributeEnabled: PropTypes.bool,
     optionalAttributesToggleStatus: PropTypes.shape({
@@ -170,7 +169,6 @@ PetitionSummary.propTypes = {
 PetitionSummary.defaultProps = {
   petition: undefined,
   petitionError: undefined,
-  petitionAttributes: [],
 };
 
 const mapStateToProps = state => ({
