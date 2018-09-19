0711bcd6c28f0986c9393f35ccb56ae67250b9e8
Elisa Cutrin <ecutrin@thoughtworks.com>
2018-09-19 17:47:32 +0200

    [+Jordi] #216 - Add signature for Zenroom contract

--- test/lib/Signature.test.js
+++ test/lib/Signature.test.js
@@ -4,6 +4,48 @@ import BuildSignatureError from '../../lib/errors/BuildSignatureError';
 describe('Signature', () => {
   const buildArray = index => Array.from({ length: 40 }, (v, i) => (index === i ? 1 : 0));
 
+  describe('representation', () => {
+    it('should return representation for vote yes with optional data', () => {
+      const vote = 'yes';
+      const age = '0-19';
+      const gender = 'm';
+
+      const actualRepresentation = new Signature(vote, age, gender).representation();
+
+      expect(actualRepresentation).toEqual('YES-M-0-19');
+    });
+
+    it('should throw an error if vote is invalid', () => {
+      const vote = 'maybe';
+      const age = 'any';
+      const gender = 'any';
+
+      const actualRepresentation = () => new Signature(vote, age, gender).representation();
+
+      expect(actualRepresentation).toThrow(BuildSignatureError);
+    });
+
+    it('should throw an error if age is invalid', () => {
+      const vote = 'yes';
+      const age = 'not an age';
+      const gender = 'any';
+
+      const actualRepresentation = () => new Signature(vote, age, gender).representation();
+
+      expect(actualRepresentation).toThrow(BuildSignatureError);
+    });
+
+    it('should throw an error if gender is invalid', () => {
+      const vote = 'yes';
+      const age = 'any';
+      const gender = 'not a gender';
+
+      const actualRepresentation = () => new Signature(vote, age, gender).representation();
+
+      expect(actualRepresentation).toThrow(BuildSignatureError);
+    });
+  });
+
   describe('binaryRepresentation', () => {
     it('should return representation for vote yes without optional data', () => {
       const vote = 'yes';
---
+++ test/lib/ZenroomContract.test.js
@@ -0,0 +1,36 @@
+import ZenroomContract from '../../lib/ZenroomContract';
+import ZenroomExecutor from '../../lib/ZenroomExecutor';
+
+import contract from '../../assets/contracts/vote';
+import Signature from '../../lib/Signature';
+
+jest.mock('../../lib/ZenroomExecutor.js');
+
+
+describe('ZenroomContract', () => {
+  describe('addSignature', () => {
+    it('should call Zenroom Executor with the correct parameters', async () => {
+      const zenroomResult = {};
+      const executeMock = jest.fn();
+      executeMock.mockResolvedValue(zenroomResult);
+
+      ZenroomExecutor.mockImplementation(() => ({
+        execute: executeMock,
+      }));
+      const zenroomExecutor = new ZenroomExecutor();
+      const zenroomContract = new ZenroomContract(zenroomExecutor);
+
+      const signature = new Signature('YES', 'ANY', 'ANY');
+      const vote = 'YES-ANY-ANY';
+      const currentOutputs = ['{"type": "PetitionEncToken"}', '{"public": "...."}'];
+      const data = {
+        public: '....',
+        option: vote,
+      };
+
+      await zenroomContract.addSignature(signature.representation(), currentOutputs);
+
+      expect(executeMock).toBeCalledWith(contract, data);
+    });
+  });
+});
