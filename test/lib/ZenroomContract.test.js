23f77b1f93cbb0914212cca4dfa73bb3e85bced7
Elisa Cutrin <ecutrin@thoughtworks.com>
2018-09-21 14:55:07 +0200

    #216 - Handle errors executing Zenroom

--- test/lib/ZenroomContract.test.js
+++ test/lib/ZenroomContract.test.js
@@ -3,12 +3,16 @@ import ZenroomExecutor from '../../lib/ZenroomExecutor';
 
 import contract from '../../assets/contracts/vote';
 import Signature from '../../lib/Signature';
+import ZenroomExecutorError from '../../lib/errors/ZenroomExecutorError';
 
 jest.mock('../../lib/ZenroomExecutor.js');
 
 
 describe('ZenroomContract', () => {
   describe('addSignature', () => {
+    const yesAnyAnySignature = new Signature('YES', 'ANY', 'ANY');
+    const currentOutputs = ['{"type": "PetitionEncToken"}', '{"public": "...."}'];
+
     it('should call Zenroom Executor with the correct parameters', async () => {
       const zenroomResult = {};
       const executeMock = jest.fn();
@@ -20,20 +24,30 @@ describe('ZenroomContract', () => {
       const zenroomExecutor = new ZenroomExecutor();
       const zenroomContract = new ZenroomContract(zenroomExecutor);
 
-      const signature = new Signature('YES', 'ANY', 'ANY');
-      const vote = 'YES-ANY-ANY';
-      const currentOutputs = ['{"type": "PetitionEncToken"}', '{"public": "...."}'];
       const data = {
         public: '....',
-        option: vote,
+        option: 'YES-ANY-ANY',
       };
 
-      await zenroomContract.addSignature(signature, currentOutputs);
+      await zenroomContract.addSignature(yesAnyAnySignature, currentOutputs);
 
       expect(executeMock).toBeCalledWith(contract, data);
     });
 
-    xit('should return an error if there is an issue executing Zenroom', () => {});
+    it('should return an error if there is an issue executing Zenroom', async () => {
+      const executeMock = jest.fn();
+      executeMock.mockRejectedValue(new Error('Failed to execute Zenroom'));
+
+      ZenroomExecutor.mockImplementation(() => ({
+        execute: executeMock,
+      }));
+      const zenroomExecutor = new ZenroomExecutor();
+      const zenroomContract = new ZenroomContract(zenroomExecutor);
+
+      await expect(zenroomContract.addSignature(yesAnyAnySignature, currentOutputs))
+        .rejects.toThrow(new ZenroomExecutorError());
+    });
+
     xit('should return a valid transaction', () => {});
   });
 });
