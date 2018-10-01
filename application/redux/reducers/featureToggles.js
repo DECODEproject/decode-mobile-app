8aa43df6bfaeeb7375b6251116515e100eeb5f8f
Raul Vega del Valle <raulvv@Rauls-MacBook-Pro.local>
2018-10-01 12:17:23 +0200

    Change IOS build number and feature toggles

--- application/redux/reducers/featureToggles.js
+++ application/redux/reducers/featureToggles.js
@@ -1,8 +1,11 @@
+import { Constants } from 'expo';
+
+const isLocal = Constants.manifest.releaseChannel !== 'production';
 
 const initialState = {
   decidimApi: false,
   zenroom: true,
-  enabledDeleteButton: true,
+  enabledDeleteButton: isLocal,
 };
 
 export default function reducer(state = initialState) {
