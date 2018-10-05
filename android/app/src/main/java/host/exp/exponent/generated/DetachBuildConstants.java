77b8c72507899dd78d11212a530cb437a38f1b2f
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-10-05 09:22:13 +0200

    eject android and test that still works

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "decodewallet://10.94.125.167:19000";
+
+}
