6c9a691a54bf619ce35e79d01ab7a6b38b53f936
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-10-19 11:52:36 +0200

    Make ios with zenroom native work

--- ios/decode-app/Zenroom.m
+++ ios/decode-app/Zenroom.m
@@ -18,26 +18,23 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(execute:(NSString *)contract data:(NSString *)data key:(NSString*)key withResolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-      RCTLogInfo(@"Zenroom.execute receiving parameters %@ %@ %@", contract, data, key);
-    contract = @"print(\"hello\")";
     
     char *c = [contract UTF8String];
     char *d = [data UTF8String];
     char *k = [key UTF8String];
     
-    size_t outputSize = 1024 * 8;
+    size_t outputSize = 1024 * 128;
     char *z_output = (char*)malloc(outputSize * sizeof(char));
-    size_t errorSize = 1024 * 8;
+    size_t errorSize = 1024 * 128;
     char *z_error = (char*)malloc(errorSize * sizeof(char));
     
-    //zenroom_exec_tobuf(c,"", k, d, 3, z_output, outputSize, z_error, errorSize);
-    zenroom_exec(c,"", k, d, 3);
+    zenroom_exec_tobuf(c, NULL, k, d, 3, z_output, outputSize, z_error, errorSize);
     
     NSLog(@"output: %s", z_output);
     NSLog(@"error: %s", z_error);
     
     
-    resolve([NSString stringWithUTF8String:stdout]);
+    resolve([NSString stringWithUTF8String: z_output]);
     
 }
 
