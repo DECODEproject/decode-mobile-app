be5a19c829f2f09709c17e8569c9587a47cb410c
Elisa Cutrin <ecutrin@thoughtworks.com>
2018-10-23 12:20:16 +0200

    211 - handle promise rejections and save credentials as string

--- application/redux/actions/credentials.js
+++ application/redux/actions/credentials.js
@@ -1,9 +1,13 @@
 
 
-export default async function setCredential(getItem, setItem, attribute) {
-  const currentCredentials = await getItem('credentials');
-
-  const allCredentials = [...currentCredentials, attribute];
-
-  await setItem('credentials', allCredentials);
+export default function setCredential(getItem, setItem, attribute) {
+  return async () => {
+    getItem('credentials').then(async (currentCredentials) => {
+      const allCredentials = [...currentCredentials, attribute];
+      await setItem('credentials', JSON.stringify(allCredentials));
+    }).catch(() => {
+      console.log('error retrieving credential');
+    });
+  };
 }
+
--- test/unit/application/redux/actions/credentials.test.js
+++ test/unit/application/redux/actions/credentials.test.js
@@ -1,3 +1,6 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import setCredential from '../../../../../application/redux/actions/credentials';
 import Attribute from '../../../../../lib/Attribute';
 
@@ -7,21 +10,32 @@ describe('setCredential', () => {
   }, '6c347975ca6aac24b46d9749808ae5392816ac23988e5dc46df4b85c0a', '');
   const setItemMock = jest.fn();
   const getItemMock = jest.fn();
+  const mockStore = configureStore([thunk]);
 
   it('should save a credential on the phone\'s local storage', async () => {
-    getItemMock.mockResolvedValue([]);
-    await setCredential(getItemMock, setItemMock, credential);
+    const initialState = {
+      credentials: [],
+    };
+    const store = mockStore(initialState);
 
-    expect(setItemMock).toBeCalledWith('credentials', [credential]);
+    getItemMock.mockResolvedValue([]);
+    await store.dispatch(setCredential(getItemMock, setItemMock, credential));
+
+    expect(setItemMock).toBeCalledWith('credentials', JSON.stringify([credential]));
   });
 
   it('should add another credential on the phone\'s local storage', async () => {
     const existingCredential = new Attribute({
       predicate: 'schema:iotCommunity', object: 'MakingSense', scope: '', provenance: { url: 'https://making-sense.eu/credential-issuer' },
     }, 'd4e8471af6492cf80afb6591bbe63f909d8d07a6efbf052b11851e5cd6', '');
+    const initialState = {
+      credentials: [],
+    };
+    const store = mockStore(initialState);
     getItemMock.mockResolvedValue([existingCredential]);
-    await setCredential(getItemMock, setItemMock, credential);
 
-    expect(setItemMock).toBeCalledWith('credentials', [existingCredential, credential]);
+    await store.dispatch(setCredential(getItemMock, setItemMock, credential));
+
+    expect(setItemMock).toBeCalledWith('credentials', JSON.stringify([existingCredential, credential]));
   });
 });
